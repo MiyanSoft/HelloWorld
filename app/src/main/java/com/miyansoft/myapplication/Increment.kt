d5e082371832f011415447e16f0b5fd5ea891cdb
Blessing <ahisublessing@gmail.com>
2021-04-25 11:58:58 -0700

    Adding a function to perform the increment operation

---
+++ app/src/main/java/com/miyansoft/myapplication/Increment.kt
@@ -0,0 +1,7 @@
+package com.miyansoft.myapplication
+
+object Increment {
+    fun SumValue(value: Int): Int {
+        return value + 1
+    }
+}
--- app/src/main/java/com/miyansoft/myapplication/MainActivity.kt
+++ app/src/main/java/com/miyansoft/myapplication/MainActivity.kt
@@ -10,12 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //get reference to the Button
         val increaseBtn = findViewById<Button>(R.id.btn_increase)
+        //make button clickable
         increaseBtn.setOnClickListener(View.OnClickListener {
+            //get reference to the TextView
             val textValue = findViewById<TextView>(R.id.txt_value)
+            //set originalValue to be the value found in the TextView
+            //since the 'text' property returns a char sequence and the needed value is an integer, there's need to convert it to a String, then to Int
             val originalValue = textValue.text.toString().toInt()
-            val newValue = originalValue + 1
+            //call function SumValue to increment the value of the original value by 1 and store it in newValue
+            val newValue = Increment.SumValue(originalValue)
+            //convert or output newValue as a string
             textValue.text = newValue.toString()
         })
 
